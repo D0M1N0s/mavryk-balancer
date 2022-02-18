1873aa8b6ac6cdbb18d35af80326773df287992c
Darya Zhukova <cherepashka2080@gmail.com>
2022-02-18 16:03:42 +0300

    added transaction cost to contract caller

--- scripts/fa12_caller_module.ts
+++ scripts/fa12_caller_module.ts
@@ -3,72 +3,70 @@ import { InMemorySigner } from '@taquito/signer'
 
 
 const acc = require('../hangzhounet.json')  // issuer's accaunt needed
-
+const PRECISION = 1000000;
 export class FA12_Caller {
-  private tezos: TezosToolkit
-  rpcUrl: string
+    private tezos: TezosToolkit
+    rpcUrl: string
 
-  constructor(rpcUrl: string) {
-    this.tezos = new TezosToolkit(rpcUrl)
-    this.rpcUrl = rpcUrl
-    //объявляем параметры с помощью метода fromFundraiser: почту, пароль и мнемоническую фразу, из которой можно получить приватный ключ
-    this.tezos.setSignerProvider(InMemorySigner.fromFundraiser(acc.email, acc.password, acc.mnemonic.join(' ')))
-  }
+    constructor(rpcUrl: string) {
+        this.tezos = new TezosToolkit(rpcUrl)
+        this.rpcUrl = rpcUrl
+        //объявляем параметры с помощью метода fromFundraiser: почту, пароль и мнемоническую фразу, из которой можно получить приватный ключ
+        this.tezos.setSignerProvider(InMemorySigner.fromFundraiser(acc.email, acc.password, acc.mnemonic.join(' ')))
+    }
 
-  public  buy_token(json_map : string, contract: string) {
-    this.tezos.contract
-    .at(contract)
-    .then((contract) => {
-      let methods = contract.parameterSchema.ExtractSignatures();
-      // console.log(JSON.stringify(methods, null, 2));
-      // console.log(json_map);
-      const {tezos_amount, token_address} = JSON.parse(json_map)
-      return contract.methods.buyToken(tezos_amount, token_address).send();
-    })
-    .then((op) => {
-      console.log(`Awaiting for ${op.hash} to be confirmed...`)
-      return op.confirmation(1).then(() => op.hash) //ждем одно подтверждение сети
-    })
-    .then((hash) => console.log(`Hash: https://hangzhou2net.tzkt.io/${hash}`)) //получаем хеш операции
-    .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`))
-  }
+    public  buy_token(json_map : string, contract: string) {
+        this.tezos.contract
+        .at(contract)
+        .then((contract) => {
+            let methods = contract.parameterSchema.ExtractSignatures();
+            
+            const {tezos_amount, token_address} = JSON.parse(json_map)
+            return contract.methods.buyToken(tezos_amount, token_address)
+                .send({amount : tezos_amount / PRECISION});    // {amount : tezos_amount}
+        })
+        .then((op) => {
+            console.log(`Awaiting for ${op.hash} to be confirmed...`)
+            return op.confirmation(1).then(() => op.hash) //ждем одно подтверждение сети
+        })
+        .then((hash) => console.log(`Hash: https://hangzhou2net.tzkt.io/${hash}`)) //получаем хеш операции
+        .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`))
+    }
 
   // moved transfering tokens in smart contract => need to delete this func
-  public transfer_tokens(standart_contract: string, sender: string, receiver: string, amount: number) {
-    this.tezos.contract
-      .at(standart_contract) //обращаемся к контракту по адресу
-      .then((contract) => {
-        console.log(`Sending ${amount} from ${sender} to ${receiver}...`)
-        //обращаемся к точке входа transfer, передаем ей адреса отправителя и получателя, а также количество токенов для отправки.
-        return contract.methods.transfer(sender, receiver, amount).send()
-      })
-      .then((op) => {
-        console.log(`Awaiting for ${op.hash} to be confirmed...`)
-        return op.confirmation(1).then(() => op.hash) //ждем одно подтверждение сети
-      })
-      .then((hash) => console.log(`Hash: https://hangzhou2net.tzkt.io/${hash}`)) //получаем хеш операции
-      .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`))
-  }
+    public transfer_tokens(standart_contract: string, sender: string, receiver: string, amount: number) {
+        this.tezos.contract
+        .at(standart_contract) //обращаемся к контракту по адресу
+        .then((contract) => {
+            console.log(`Sending ${amount} from ${sender} to ${receiver}...`)
+            //обращаемся к точке входа transfer, передаем ей адреса отправителя и получателя, а также количество токенов для отправки.
+            return contract.methods.transfer(sender, receiver, amount).send()
+        })
+        .then((op) => {
+            console.log(`Awaiting for ${op.hash} to be confirmed...`)
+            return op.confirmation(1).then(() => op.hash) //ждем одно подтверждение сети
+        })
+        .then((hash) => console.log(`Hash: https://hangzhou2net.tzkt.io/${hash}`)) //получаем хеш операции
+        .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`))
+    }
 
-  public async open_sale(json_map : string, contract : string) {
-    //let open = async() => {
-      this.tezos.contract
-      .at(contract)
-      .then((c) => {
-        let methods = c.parameterSchema.ExtractSignatures();
-        // console.log(JSON.stringify(methods, null, 2));
-        console.log("Opening sale");
-        const {total_token_amount, token_address, close_date, token_weight} = JSON.parse(json_map);
-        
-        return c.methods.openSale(token_address, total_token_amount, close_date, token_weight, 100 - token_weight).send();  
-      })
-      .then((op) => {
-        console.log(`Awaiting for ${op.hash} to be confirmed...`)
-        return op.confirmation(1).then(() => op.hash) //ждем одно подтверждение сети
-      })
-      .then((hash) => console.log(`Hash: https://hangzhou2net.tzkt.io/${hash}`)) //получаем хеш операции
-      .catch((error) => console.log(`Error 1: ${JSON.stringify(error, null, 2)}`))
-    //}
-    //await async() => { open()};
-  }
+    public async open_sale(json_map : string, contract : string) {
+        this.tezos.contract
+        .at(contract)
+        .then((c) => {
+            let methods = c.parameterSchema.ExtractSignatures();
+            // console.log(JSON.stringify(methods, null, 2));
+            console.log("Opening sale");
+            const {total_tezos_amount, total_token_amount, token_address, close_date, token_weight} = JSON.parse(json_map);
+            // let params = SendP
+            return c.methods.openSale(token_address, total_token_amount, close_date, token_weight, 100 - token_weight)
+            .send({amount : total_tezos_amount / PRECISION});  // here need to put the amnt of tez to put on contract
+        })
+        .then((op) => {
+            console.log(`Awaiting for ${op.hash} to be confirmed...`)
+            return op.confirmation(1).then(() => op.hash) //ждем одно подтверждение сети
+        })
+        .then((hash) => console.log(`Hash: https://hangzhou2net.tzkt.io/${hash}`)) //получаем хеш операции
+        .catch((error) => console.log(`Error 1: ${JSON.stringify(error, null, 2)}`))
+    }
 }
--- scripts/maincall.ts
+++ scripts/maincall.ts
@@ -1,65 +1,39 @@
 import { FA12_Caller } from './fa12_caller_module'
 
 const RPC_URL_ = 'https://rpc.tzkt.io/hangzhou2net/'
-const STANDART_CONTRACT = 'KT1LDjRzueHme3nDQGC9irB7MtWDVstm3ebC' //адрес опубликованного контракта стандарта токена
+const TOKEN_STANDART_CONTRACT = 'KT1LDjRzueHme3nDQGC9irB7MtWDVstm3ebC' //адрес опубликованного контракта стандарта токена
 const SENDER_ = 'tz1MtDKdcXrk4s9aMA73uVjCSFpd1WEqRM2H' //публичный адрес отправителя (эмитента)
 const RECEIVER_ = 'KT1Pbmd9czwAP2eFhriQdWLFMwkAGPSUyRTu' //публичный адрес получателя (смарт контракта для токенсейла)
 const TOKEN_AMOUNT = 1 //количество токенов для отправки.
 
 
 const RPC_URL = 'https://rpc.tzkt.io/hangzhou2net/'
-const CONTRACT = 'KT1DGds71osaXGr2AxGcdMFTyENM76MjkqoJ' //адрес контракта для токенсейла с балансером
+const TOKENSALE_CONTRACT = 'KT1QrGJeXK96C9uMCb7vtFdCgHYuRSJdboqS' //адрес контракта для токенсейла с балансером
 // const RECEIVER = 'tz1MtDKdcXrk4s9aMA73uVjCSFpd1WEqRM2H'
 
 let open_sale_json_map = `{ 
             "total_token_amount" : 2,
             "close_date" : "2022-01-01T00:01:30Z",
             "token_weight" : 80,
-            "token_address" : "${STANDART_CONTRACT}"
+            "total_tezos_amount" : 2,
+            "token_address" : "${TOKEN_STANDART_CONTRACT}"
             }`
 let buy_token_json_map = `{
             "tezos_amount" : 2, 
-            "token_address" : "${STANDART_CONTRACT}"
+            "token_address" : "${TOKEN_STANDART_CONTRACT}"
             }`
 let caller = new FA12_Caller(RPC_URL)
 // todo: to make contract calls async, currently it goes parallel so one operation out of two (three) fails
 
 const open_sale = async () => {
-    await caller.open_sale(open_sale_json_map, CONTRACT);
+    await caller.open_sale(open_sale_json_map, TOKENSALE_CONTRACT);
 };
-open_sale();
 const buy_token = async () => {
-    await caller.buy_token(buy_token_json_map, CONTRACT);
+    await caller.buy_token(buy_token_json_map, TOKENSALE_CONTRACT);
 };
-buy_token();
+open_sale()
 
-// caller.buy_token(buy_token_json_map, CONTRACT);
-// caller.transfer_tokens(STANDART_CONTRACT, SENDER_, RECEIVER_, TOKEN_AMOUNT)
+// buy_token();
 
-
-
-
-import { importKey } from '@taquito/signer'
-import { TezosToolkit } from '@taquito/taquito';
-import { InMemorySigner } from '@taquito/signer'
-
-
-const acc = require('../hangzhounet.json')  // issuer's accaunt needed
-
-
-function send_tezos() {
-    // dont work sending to contract....
-    const Tezos = new TezosToolkit(RPC_URL); //'https://hangzhounet.api.tez.ie');
-    const amount = 0.005;
-    const address = CONTRACT;
-    Tezos.setSignerProvider(InMemorySigner.fromFundraiser(acc.email, acc.password, acc.mnemonic.join(' ')))
-    console.log(`Transfering ${amount} ꜩ to ${address}`);
-    Tezos.contract.transfer({ to: address, amount: amount })
-            .then((op) => {
-            console.log(`Waiting for ${op.hash} to be confirmed...`);
-            return op.confirmation(1).then(() => op.hash);
-          })
-          .then((hash) => console.log(`Operation injected: https://hangzhou.tzstats.com/${hash}`))
-          .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
-}
-          
+// caller.buy_token(buy_token_json_map, TOKENSALE_CONTRACT);
+// caller.transfer_tokens(TOKEN_STANDART_CONTRACT, SENDER_, RECEIVER_, TOKEN_AMOUNT)
