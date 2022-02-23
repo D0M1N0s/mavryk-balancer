20fbf86ebe3e11ec8ef1168f0a245bb012d14779
Darya Zhukova <cherepashka2080@gmail.com>
2022-02-23 18:28:47 +0300

    updated scripts for compilation and deployment

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,3 +2,4 @@
 # should be started from project dirictory
 mkdir -p build
 ligo compile contract  "smart-contracts/$1" > "build/$2"
+echo "Smart contract was compiled into \"build/\" folder"
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,43 +2,38 @@ import { TezosToolkit } from '@taquito/taquito'
 import { importKey } from '@taquito/signer'
 import { MichelsonMap } from '@taquito/taquito';
 
-
 const fs = require("fs");
 const provider = 'https://rpc.tzkt.io/hangzhou2net/'
 const { email, password, mnemonic, activation_code } = JSON.parse(fs.readFileSync('../hangzhounet.json').toString())
+const FA2_compiled_code = "../build/fa2_swapper.tz"
+const FA12_compiled_code = "../build/fa12_swapper.tz"
 
+async function deploy(path) {
+    const tezos = new TezosToolkit(provider)
+    await importKey(
+        tezos,
+        email, 
+        password, 
+        mnemonic.join(' '),
+        activation_code
+    )
 
-async function deploy() {
-  const tezos = new TezosToolkit(provider)
-  await importKey(
-    tezos,
-    email, 
-    password, 
-    mnemonic.join(' '),
-    activation_code //приватный ключ
-  )
+    try {
+        const op = await tezos.contract.originate({
+            code: fs.readFileSync(path, "utf-8").toString(),
+            storage: new MichelsonMap(),
+            balance : '0.3' // initial contract balance
+        })
 
-  try {
-    const op = await tezos.contract.originate({
-        //код смарт-контракта
-        code: fs.readFileSync("../build/fa12_swapper.tz", "utf-8").toString(),
-        //значение хранилища
-        storage: new MichelsonMap(),
-        balance : '0.3' // initial contract balance
-    })
-
-    //начало развертывания
-    console.log('Awaiting confirmation...')
-    const contract = await op.contract()
-    //отчет о развертывании: количество использованного газа, значение хранилища
-    console.log('Gas Used', op.consumedGas)
-    console.log('Storage', await contract.storage())
-    //хеш операции, по которому можно найти контракт в блокчейн-обозревателе
-    console.log('Operation hash:', op.hash)
-    console.log("Search originated contract on https://hangzhou2net.tzkt.io")
-  } catch (ex) {
-    console.error(ex)
-  }
+        console.log('Awaiting confirmation...')
+        const contract = await op.contract()
+        console.log('Gas Used', op.consumedGas)
+        console.log('Storage', await contract.storage())
+        console.log('Operation hash:', op.hash)
+        console.log("Search originated contract on https://hangzhou2net.tzkt.io")
+    } catch (ex) {
+        console.error(ex)
+    }
 }
 
-deploy()
+deploy(FA2_compiled_code)
