27a766dab7a221d606a24b06e3c2ff0360b44c3d
Darya Zhukova <cherepashka2080@gmail.com>
2022-03-03 00:20:55 +0300

    modified compiling script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # should be started from project dirictory
 mkdir -p build
-ligo compile contract  "smart-contracts/$1" > "build/$2"
-echo "Smart contract was compiled into \"build/\" folder"
+file_name=(${1//./ })   
+result=`ligo compile contract  "smart-contracts/$1" > "build/${file_name[0]}.tz"`
